18db46a2405fb6b151254ce9b67609d15cc74cd8
ethanruity <eruity1@gmail.com>
2021-11-21 14:47:31 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+$("#ones").click(() => {
+  $("#ones-drop").slideToggle("slow");
+});
+
+$("#threes").click(() => {
+  $("#threes-drop").slideToggle("slow");
+});
+
+$("#red").click(() => {
+  me.src = "imgs/me-one.png";
+});
+
+$("#purple").click(() => {
+  me.src = "imgs/purple-ones.png";
+});
+
+$("#blue").click(() => {
+  me.src = "imgs/blue-ones.png";
+});
+
+$("#green").click(() => {
+  me.src = "imgs/green-ones.png";
+});
+
+$("#white").click(() => {
+  me.src = "imgs/white-threes.png";
+});
+
+$("#black").click(() => {
+  me.src = "imgs/black-threes.png";
+});
+
+$("#fe").click(() => {
+  $("#fe-logo").slideToggle("fast");
+});
+
+$("#be").click(() => {
+  $("#be-logo").slideToggle("fast");
+});
+
+$("#db").click(() => {
+  $("#db-logo").slideToggle("fast");
+});
+
+$("#ot").click(() => {
+  $("#ot-logo").slideToggle("fast");
+});
